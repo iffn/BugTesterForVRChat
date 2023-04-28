eba317fba12a68334c77453639db26d0b454938f
iffn <iffn1990@gmail.com>
2023-04-28 16:22:21 +0200

    Implemented image loader test

---
+++ Scripts/Tests/ImageLoader/ImageLoaderTest.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Image;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+using UnityEngine.UI;
+
+
+public class ImageLoaderTest : UdonSharpBehaviour
+{
+    [SerializeField] 
+    VRCImageDownloader currentImageDownloader;
+
+    [SerializeField] Material linkedMaterial;
+    [SerializeField] VRCUrl url;
+    [SerializeField] Image linkedImage;
+
+    void Start()
+    {
+        currentImageDownloader = new VRCImageDownloader();
+
+        currentImageDownloader.DownloadImage(url, linkedMaterial, (IUdonEventReceiver)this);
+    }
+
+    public override void OnImageLoadSuccess(IVRCImageDownload result)
+    {
+        //Set material to image after load to refresh Unity image
+        linkedImage.material = linkedMaterial;
+    }
+}
