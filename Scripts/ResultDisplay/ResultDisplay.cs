407f9b410b01621e850661478af7ce8b3795aa4a
iffn <iffn1990@gmail.com>
2023-05-03 17:23:52 +0200

    Created basic ResultDisplay

---
+++ Scripts/ResultDisplay/ResultDisplay.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class ResultDisplay : UdonSharpBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI Description;
+    [SerializeField] Image MainState; 
+    [SerializeField] Image ClientSimState; 
+    [SerializeField] Image DesktopState; 
+    [SerializeField] Image PCVRState; 
+    [SerializeField] Image QuestState;
+
+    [SerializeField] Color ExpectedPassColor;
+    [SerializeField] Color SurprisePassColor;
+    [SerializeField] Color ExpectedFailColor;
+    [SerializeField] Color SurpriseFailColor;
+
+    void Start()
+    {
+        
+    }
+}
